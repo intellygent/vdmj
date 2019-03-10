739db63b1c2da25ee29abd580909d20bc2ce08a7
paulch42 <paul@10.0.1.5>
2019-03-10 19:53:59 +1100

    Default extent and toHTML methods

--- DocPlugin/src/main/java/plugins/doc/DOCNode.java
+++ DocPlugin/src/main/java/plugins/doc/DOCNode.java
@@ -1,4 +1,4 @@
-/*******************************************************************************
+/** *****************************************************************************
  *
  *	Copyright (c) 2016 Fujitsu Services Ltd.
  *
@@ -19,8 +19,8 @@
  *	You should have received a copy of the GNU General Public License
  *	along with VDMJ.  If not, see <http://www.gnu.org/licenses/>.
  *
- ******************************************************************************/
-
+ *****************************************************************************
+ */
 package plugins.doc;
 
 import java.io.Serializable;
@@ -28,34 +28,33 @@ import java.io.Serializable;
 import com.fujitsu.vdmj.mapper.MappedObject;
 
 /**
- * The abstract root of all TC nodes.
+ * The abstract root of all DOC nodes.
  */
-abstract public class DOCNode extends MappedObject implements Serializable
-{
-	private static final long serialVersionUID = 1L;
-	public final static String MAPPINGS = "tc-doc.mappings";
+abstract public class DOCNode extends MappedObject implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    public final static String MAPPINGS = "tc-doc.mappings";
     public Extent extent; // Extent of the content when formatted.
-	
-	protected DOCNode()
-	{
-		super();
-	}
-    
-    // Set the extent (width and height) of a term.
-    public void extent(int maxWidth)
-    {
-    }
-    
-    // Generate the HTML format of a term.
-    public String toHTML(int indent)
-    {
-        return null;
-    }
-    
-    // Generate HTML format of an expression when its width is not significant.
-    public String toHTML()
-    {
-        return toHTML(0);
+
+    protected DOCNode() {
+        super();
     }
 
+    // Set the extent (width and height) of a term.
+    //public abstract void extent(int maxWidth);
+    // Default method to allow compilation before implementation complete.
+    public void extent(int maxWidth) {
+    }
+
+    // Generate the HTML format of a term.
+    //public abstract String toHTML(int indent);
+    // Default method to allow compilation before implementation complete.
+    public String toHTML(int indent) {
+        return null;
+    }
+
+    // Generate HTML format of an expression when its width is not significant.
+    public String toHTML() {
+        return toHTML(0);
+    }
 }
